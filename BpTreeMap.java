0f3e7d2ec096ac29aad7569c9c1fae99463a3c24
Kyle Millar <kymilla608@gmail.com>
2017-10-08 23:53:31 -0400

    Update BpTreeMap.java

--- BpTreeMap.java
+++ BpTreeMap.java
@@ -52,7 +52,7 @@ public class BpTreeMap <K extends Comparable <K>, V>
     /********************************************************************************
      * This inner class defines nodes that are stored in the B+tree map.
      */
-    private class Node
+    private class Node implements Serializable
     {
         boolean   isLeaf;                             // whether the node is a leaf 
         int       nKeys;                              // number of active keys
